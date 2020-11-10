bf1b7a6b554f9cef6386f9d8167c62dffc38569d
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-11-10 20:13:46 -0300

    Operacoes do Linq

---
+++ OperacoesDoLINQ/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Course.Entities
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
